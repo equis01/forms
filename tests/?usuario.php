fd4de82a640e27598175942d20043c5842cb541a
equis01 <71985026+equis01@users.noreply.github.com>
2021-01-12 21:20:33 -0600

    Rename usuario.php to ?usuario.php

--- tests/?usuario.php
+++ tests/?usuario.php
@@ -5,7 +5,7 @@
    if (($user == "usuario") AND ($password == "12345")) {
       echo "Bienvenido ".$user;
    } else {
-      echo "¡Usuario o contraseña incorrectos!";
+      echo "Â¡Usuario o contraseÃ±a incorrectos!";
       echo '<br><a href="'.$_SERVER['HTTP_REFERER'].'">Volver</a>';
    }
 ?>
